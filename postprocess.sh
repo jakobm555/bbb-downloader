818732ef052608b45a669b1bc766251e903f9e48
Jakob Mischke <jakobmis08@gmail.com>
2021-03-24 15:16:50 +0100

    enhancements

--- postprocess.sh
+++ postprocess.sh
@@ -11,7 +11,7 @@ OPTIONS:
    -l latency        offset betqween video and audio in seconds
    -o output_file    Output file
    -p true           Presentation only
-   -c true           Copy (faster conversion but keyframes)
+   -c true           Copy (faster conversion but keyframe problem at beginning)
 EOF
 }
 
@@ -42,21 +42,25 @@ if [ $# -lt 1 ]; then
     exit 2
 fi
 
-#only the main presentation screen
-if [ "$presentation_only" = "true" ]; then
-    if [ "$copy" = "true" ]; then
-        echo "Attention! no copy mode with cropping possible. Switching -c to false." && copy=false
-    fi
-    filter='-filter:v crop=1280:880:0:0'
-fi
-
 # use copy for faster conversion
 if [ "$copy" = "true" ]; then
     codec='-c:a copy -c:v copy'
 else
     codec='-c:a aac -c:v libx264'
+    afilter='-filter:a aresample=async=1'
 fi
-    
-echo "executing: ffmpeg -i $1 $latency -i $1 $codec $filter -map 0:v:0 -map 1:a:0 $start $duration $output"
 
-ffmpeg -i $1 $latency -i $1 $codec $filter -map 0:v:0 -map 1:a:0 $start $duration $output
+
+
+#only the main presentation screen
+if [ "$presentation_only" = "true" ]; then
+    if [ "$copy" = "true" ]; then
+        echo "Attention! no copy mode with cropping possible. Switching -c to false." && copy=false
+    fi
+    vfilter="-filter:v crop=1280:880:0:0"
+fi
+
+    
+echo "executing: ffmpeg -i $1 $latency -i $1 $codec $afilter $vfilter  -map 0:v:0 -map 1:a:0 $start $duration $output"
+
+ffmpeg -i $1 $latency -i $1 $codec $afilter $vfilter -map 0:v:0 -map 1:a:0 $start $duration $output
